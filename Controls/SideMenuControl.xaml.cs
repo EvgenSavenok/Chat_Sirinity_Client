6e182397de99cdbedf04cda0dca2741372e00f40
EvgenSavenok <eugen.savenok@gmail.com>
2024-05-14 20:00:29 +0300

    Feat: first chat (frontend)

---
+++ Controls/SideMenuControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Chat_Sirinity_Client.Pages;
+
+public partial class SideMenuControl : UserControl
+{
+    public SideMenuControl()
+    {
+        InitializeComponent();
+    }
+}
+
