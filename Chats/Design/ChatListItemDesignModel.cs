7700cbe83dc8d721f2ed5f13048fcfb218c22a08
EvgenSavenok <eugen.savenok@gmail.com>
2024-05-14 21:28:28 +0300

    Feat: now i can change displaying of list of chats.

---
+++ Chats/Design/ChatListItemDesignModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Media;
+
+namespace Chat_Sirinity_Client.Chats.Design;
+
+public class ChatListItemDesignModel : ChatListItemViewModel
+{
+    #region Singleton
+
+    public static ChatListItemDesignModel DesignInstance => new();
+
+    #endregion
+
+    #region Constructor
+
+    public ChatListItemDesignModel()
+    {
+        Initial = "LM";
+        Name = "Luke";
+        Message = "This chat is amazing!";
+        ProfilePictureRGB = "3099c5";
+    }
+
+    #endregion
+}
---
+++ Convertors/StringRGBToBrushConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Windows.Media;
+
+namespace Chat_Sirinity_Client.Convertors;
+
+public class StringRGBToBrushConverter : BaseValueConvertor<StringRGBToBrushConverter>
+{
+    public override object Convert(object value, Type targerType, object parameter, CultureInfo culture)
+    {
+        return (SolidColorBrush)new BrushConverter().ConvertFrom($"#{value}")!;
+    }
+
+    public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new Exception();
+    }
+}
